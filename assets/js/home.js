9dfec927bbbde6220215cd862373160917d99514
Wahaj Arsal <wahaj@Mac.localdomain>
2025-10-08 15:22:30 +0100

    facts section added with js

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,6 +5,11 @@ const getSecondTile = document.querySelector('#secondTile')
 const getThirdTile = document.querySelector('#thirdTile')
 const getFourthTile = document.querySelector('#fourthTile')
 
+const getMessage = document.querySelector('#message')
+const getOrganisation = document.querySelector('#organisation')
+const getTechnical = document.querySelector('#technical')
+const factsButton = document.querySelector('#facts-button')
+
 getFirstTile.addEventListener('click', () => {
     console.log('First Tile Clicked')
     galleryImage.src = './assets/images/food/pancakes.jpg'
@@ -36,4 +41,11 @@ getFourthTile.addEventListener('click', () => {
     getSecondTile.classList.remove('options__tile-active')
     getThirdTile.classList.remove('options__tile-active')
     getFourthTile.classList.add('options__tile-active')
+})
+
+factsButton.addEventListener('click', () => {
+    console.log('Facts Button Clicked')
+    getMessage.classList.toggle('facts__single-card-hidden')
+    getOrganisation.classList.toggle('facts__single-card-hidden')
+    getTechnical.classList.toggle('facts__single-card-hidden')
 })
